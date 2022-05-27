60ad6f902ac8f11fa02488234d40863921330bfc
andrei <ablajievschii@syncast.ai>
2022-05-27 16:30:18 -0700

    Initial commit with partially working solution

---
+++ app/src/main/java/com/example/android/treechartapplication/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.android.treechartapplication
+
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.Button
+import android.widget.FrameLayout
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val testData = intArrayOf(2, 3, 4)
+        val parent = TreeChart(this).apply {
+            setBackgroundColor(Color.argb(
+                255,
+                240,
+                240,
+                240,
+            ))
+            layoutParams = LinearLayout.LayoutParams(
+                resources.getDimensionPixelSize(R.dimen.size),
+                resources.getDimensionPixelSize(R.dimen.size)
+            )
+        }
+        parent.setData(testData)
+
+        val button = Button(this).apply {
+            text = "random data"
+            setOnClickListener {
+                parent.setData(IntArray(10) { (Math.random() * 10 + 1).toInt() })
+//                parent.setData(testData)
+            }
+        }
+        findViewById<LinearLayout>(R.id.container).apply {
+            addView(parent)
+            addView(button)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/android/treechartapplication/TreeChart.kt
@@ -0,0 +1,177 @@
+package com.example.android.treechartapplication
+
+import android.content.Context
+import android.graphics.Color
+import android.graphics.Rect
+import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.TextView
+import kotlin.random.Random
+
+class TreeChart @JvmOverloads constructor(
+    private val ctx: Context,
+    private val attr: AttributeSet? = null,
+    private val defStyle: Int = 0
+) : FrameLayout(ctx, attr, defStyle) {
+
+    private var data = intArrayOf()
+    private val dataMap = mutableMapOf<View, Rect>()
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+
+        // FIXME assume fixed size
+        val width = MeasureSpec.getSize(widthMeasureSpec)
+        val height = MeasureSpec.getSize(heightMeasureSpec)
+
+        renderChildren(data,0, 0, width, height)
+        for (i in 0..childCount) {
+            val view = getChildAt(i)
+            val rect = dataMap[view]
+            rect?.let {
+                val widthSpec = MeasureSpec.makeMeasureSpec(rect.width(), MeasureSpec.EXACTLY)
+                val heightSpec = MeasureSpec.makeMeasureSpec(rect.height(), MeasureSpec.EXACTLY)
+                view.measure(widthSpec, heightSpec)
+            }
+        }
+
+    }
+
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        super.onLayout(changed, left, top, right, bottom)
+
+        for (i in 0 until childCount) {
+            val view = getChildAt(i)
+            val rect = dataMap[view]
+            rect?.let {
+                view.layout(
+                    it.left,
+                    it.top,
+                    it.left + it.width(),
+                    it.top + it.height()
+                )
+            }
+        }
+    }
+
+    fun setData(testData: IntArray) {
+        data = testData
+        dataMap.clear()
+        removeAllViews()
+//        invalidate()
+//        requestLayout()
+    }
+
+    /**
+     * @param x position within the parent
+     * @param y position within the parent
+     * @param width current arr group width
+     * @param height current arr group height
+     */
+    private fun renderChildren(arr: IntArray, x: Int, y: Int, width: Int, height: Int) {
+        // base case
+        if (arr.isEmpty()) return
+        if (arr.size <= 2) {
+            addChildren(arr, x, y, width, height)
+            return
+        }
+
+        val total = arr.sum()
+        val halfSum = total / 2
+        val mid = getMidPointIndex(arr, halfSum)
+        val left = arr.copyOfRange(0, mid)
+        val right = arr.copyOfRange(mid, arr.size)
+        println("mid=$mid, arr.size=${arr.size}")
+        if (width >= height) {
+            // split horizontally
+            val halfWidth = (halfSum.toFloat() / total * width).toInt()
+
+            renderChildren(left, x, y, halfWidth, height)
+            renderChildren(right, x + halfWidth, y, width - halfWidth, height)
+        } else {
+            // split vertically
+            val halfHeight = (halfSum.toFloat() / total * height).toInt()
+
+            // left -> top, right -> bottom
+            renderChildren(left, x, y, width, halfHeight)
+            renderChildren(right, x, y + halfHeight, width, height - halfHeight)
+        }
+    }
+
+    private fun getMidPointIndex(arr: IntArray, halfSum: Int): Int {
+        var summ = 0
+        var mid = 1
+        for (i in arr.indices) {
+            if (summ >= halfSum) {
+                mid = i - 1
+                break
+            }
+            summ += arr[i]
+        }
+
+        return if (mid <= 0)
+            1 else mid
+    }
+
+    private fun addChildren(arr: IntArray, x: Int, y: Int, width: Int, height: Int) {
+        val total = arr.sum().toFloat()
+        if (width >= height) {
+            // place horizontally
+            var usedWidth: Int = 0
+            arr.forEach { value ->
+                val itemWidth = (value / total * width).toInt()
+
+                val newX = x + usedWidth
+                addChild(getChildView(value), Rect(
+                    newX,
+                    y,
+                    newX + itemWidth,
+                    y + height
+                ))
+                usedWidth += itemWidth
+            }
+        } else {
+            // place vertically
+            var usedHeight: Int = 0
+            arr.forEach { value ->
+                val itemHeight = (value / total * height).toInt()
+
+                val newY = y + usedHeight
+                addChild(getChildView(value), Rect(
+                    x,
+                    newY,
+                    x + width,
+                    newY + itemHeight
+                ))
+                usedHeight += itemHeight
+            }
+        }
+    }
+
+    private fun addChild(view: View, rect: Rect) {
+        addView(view)
+        dataMap[view] = rect
+    }
+
+    private fun getChildView(value: Int): View {
+        return TextView(context).apply {
+            text = value.toString()
+            setTextSize(TypedValue.COMPLEX_UNIT_SP, 24f)
+            setBackgroundColor(
+                Color.argb(
+                    255,
+                    Random.nextInt(256),
+                    Random.nextInt(256),
+                    Random.nextInt(256),
+                )
+            )
+            layoutParams = FrameLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+        }
+    }
+}
